4a804c61b1cf1589841af30ce3549861db5bf63d
Peace Williams-Ojomu <peaceojomu@gmail.com>
2026-01-13 18:59:20 +0000

    Create search bar

--- src/components/PaymentsPage.tsx
+++ src/components/PaymentsPage.tsx
@@ -1,5 +1,7 @@
 import {
   Container,
+  FilterRow,
+  SearchButton,
   SearchInput,
   Spinner,
   StatusBadge,
@@ -18,57 +20,78 @@ import { API_URL } from "../constants";
 import { I18N } from "../constants/i18n";
 import { Payment, PaymentSearchResponse } from "../types/payment";
 import { formatPaymentDate, formatCurrency } from "../utils/formatters";
+import { useState } from "react";
 
 export const PaymentsPage = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [activeSearch, setActiveSearch] = useState("");
   const { isPending, data } = useQuery<PaymentSearchResponse>({
-    queryKey: ["payments", { page: 1, pageSize: 5 }],
-    queryFn: () =>
-      fetch(`${API_URL}?page=1&pageSize=5`).then((res) => res.json()),
+    queryKey: ["payments", { page: 1, pageSize: 5, search: activeSearch }],
+    queryFn: async () => {
+      const params = new URLSearchParams({
+        page: "1",
+        pageSize: "5",
+        ...(activeSearch && { search: activeSearch }),
+      });
+      return fetch(`${API_URL}?${params}`).then((res) => res.json());
+    },
   });
 
-  if (isPending)
-    return (
-      <Container>
-        <Spinner />
-      </Container>
-    );
+  const handleSearch = () => {
+    setActiveSearch(searchTerm);
+  };
 
   return (
     <Container>
       <Title>{I18N.PAGE_TITLE}</Title>
 
-      <SearchInput placeholder={I18N.SEARCH_PLACEHOLDER} />
-      <TableWrapper>
-        <Table>
-          <TableHeaderWrapper>
-            <TableHeaderRow>
-              <TableHeader>{I18N.TABLE_HEADER_PAYMENT_ID}</TableHeader>
-              <TableHeader>{I18N.TABLE_HEADER_DATE}</TableHeader>
-              <TableHeader>{I18N.TABLE_HEADER_AMOUNT}</TableHeader>
-              <TableHeader>{I18N.TABLE_HEADER_CUSTOMER}</TableHeader>
-              <TableHeader>{I18N.TABLE_HEADER_CURRENCY}</TableHeader>
-              <TableHeader>{I18N.TABLE_HEADER_STATUS}</TableHeader>
-            </TableHeaderRow>
-          </TableHeaderWrapper>
+      <FilterRow>
+        <SearchInput
+          type="text"
+          placeholder={I18N.SEARCH_PLACEHOLDER}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          role="searchbox"
+          aria-label={I18N.SEARCH_LABEL}
+        />
+        <SearchButton onClick={handleSearch}>{I18N.SEARCH_BUTTON}</SearchButton>
+      </FilterRow>
 
-          <TableBodyWrapper>
-            {data?.payments?.map((payment: Payment) => (
-              <TableRow key={payment.id}>
-                <TableCell>{payment.id}</TableCell>
-                <TableCell>{formatPaymentDate(payment.date)}</TableCell>
-                <TableCell>{formatCurrency(payment.amount)}</TableCell>
-                <TableCell>{payment.customerName}</TableCell>
-                <TableCell>{payment.currency}</TableCell>
-                <TableCell>
-                  <StatusBadge status={payment.status}>
-                    {payment.status}
-                  </StatusBadge>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBodyWrapper>
-        </Table>
-      </TableWrapper>
+      {isPending ? (
+        <Spinner />
+      ) : (
+        <TableWrapper>
+          <Table>
+            <TableHeaderWrapper>
+              <TableHeaderRow>
+                <TableHeader>{I18N.TABLE_HEADER_PAYMENT_ID}</TableHeader>
+                <TableHeader>{I18N.TABLE_HEADER_DATE}</TableHeader>
+                <TableHeader>{I18N.TABLE_HEADER_AMOUNT}</TableHeader>
+                <TableHeader>{I18N.TABLE_HEADER_CUSTOMER}</TableHeader>
+                <TableHeader>{I18N.TABLE_HEADER_CURRENCY}</TableHeader>
+                <TableHeader>{I18N.TABLE_HEADER_STATUS}</TableHeader>
+              </TableHeaderRow>
+            </TableHeaderWrapper>
+
+            <TableBodyWrapper>
+              {data?.payments?.map((payment: Payment) => (
+                <TableRow key={payment.id}>
+                  <TableCell>{payment.id}</TableCell>
+                  <TableCell>{formatPaymentDate(payment.date)}</TableCell>
+                  <TableCell>{formatCurrency(payment.amount)}</TableCell>
+                  <TableCell>{payment.customerName}</TableCell>
+                  <TableCell>{payment.currency}</TableCell>
+                  <TableCell>
+                    <StatusBadge status={payment.status}>
+                      {payment.status}
+                    </StatusBadge>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBodyWrapper>
+          </Table>
+        </TableWrapper>
+      )}
     </Container>
   );
 };
